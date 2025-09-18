fbcca42ef98d11152810698ec2f4f3de7e3f699d
anchellon <anshulmathew@gmail.com>
2025-09-17 22:22:32 -0700

    Added refresh scope to the controller

--- department-service/src/main/java/com/example/department/web/DepartmentController.java
+++ department-service/src/main/java/com/example/department/web/DepartmentController.java
@@ -5,6 +5,7 @@ import com.example.department.service.DepartmentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/api/v1/departments")
 @RequiredArgsConstructor
 @Validated
